7bfe8c5b8845ce77fb1e0288f95e815e861c781e
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-07-19 03:54:27 +0530

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://leaderboard-production-3a27.up.railway.app",
     credentials: true,
   })
 );
